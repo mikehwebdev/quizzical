5167027782eb457a61e4e81dfe71be9f6c3250ae
mikehwebdev <81847609+mikehwebdev@users.noreply.github.com>
2023-03-15 14:17:03 +0000

    Adjusting main app class viewport

--- data.js
+++ data.js
@@ -34,37 +34,6 @@ export default  [
     answers:[{answer:"One", isToggled: false}, {answer:"Two", isToggled: false}, {answer:"Three", isToggled: false}, {answer:"Four", isToggled: false}],
     correctAnswer:"Three",
     id:4
-},
-{
-    question:"How would one say goodbye in Spanish?",
-    answers:[{answer:"Adiós", isToggled: false}, {answer:"Hola", isToggled: false},{answer: "Au Revoir", isToggled: false},{answer:"Salir", isToggled: false}],
-    correctAnswer: "Adiós",
-    id:0
-},
-
-{
-    question:"Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?",
-    answers:[{answer:"Cabbage Patch Kids", isToggled: false}, {answer:"Transformers", isToggled: false}, {answer:"Care Bears", isToggled: false}, {answer:"Rubik’s Cube",isToggled: false}],
-    correctAnswer:"Care Bears",
-    id:1
-},
-{
-    question:"What is the hottest planet in our Solar System?",
-    answers:[{answer:"Mercury", isToggled: false}, {answer: "Venus",isToggled: false}, {answer:"Mars", isToggled: false}, {answer:"Saturn", isToggled: false}],
-    correctAnswer:"Venus",
-    id:2
-},
-{
-    question:"In which country was the caesar salad invented?",
-    answers:[{answer:"Italy", isToggled: false}, {answer:"Portugal",isToggled: false}, {answer: "Mexico", isToggled: false}, {answer:"France", isToggled: false}],
-    correctAnswer:"Mexico",
-    id:3
-},
-{
-    question:"How Many Hearts Does An Octopus Have?",
-    answers:[{answer:"One", isToggled: false}, {answer:"Two", isToggled: false}, {answer:"Three", isToggled: false}, {answer:"Four", isToggled: false}],
-    correctAnswer:"Three",
-    id:4
 }
 
 ]
